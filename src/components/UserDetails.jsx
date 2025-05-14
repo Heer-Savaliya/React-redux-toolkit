df755851051d993b3dc895e1c622b11b3539cad5
Heer Savaliya <heersavaliya65@gmail.com>
2025-05-14 14:49:26 +0530

    user component

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -3,18 +3,20 @@ import DeleteAllUser from './DeleteAllUser'
 
 const UserDetails = () => {
   return (
-    <div>
+    <div className='w-full md:w-[80%] lg:w-[50%]  mx-auto'>
       {/* Admin table */}
       <div>
-      <div>
-        <div>List of User Details</div>
-        <button>Add New Users</button>
+      <div className='flex items-center justify-between my-5'>
+        <div className='text-gray-500 font-semibold font-serif text-2xl'>List of User Details</div>
+        <button className='px-5 py-2 rounded-[12px] text-white bg-linear-to-t from-sky-500 to-indigo-500'>Add New Users</button>
       </div>
+      <hr className='text-gray-400'/>
+      <br />
       <ul>
         {/* <li>Hii</li>
         <li>Hyyyy</li> */}
       </ul>
-      <hr />
+      <hr/>
       <DeleteAllUser />
       </div>
     </div>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -11,15 +11,15 @@ const Navbar = () => {
                             
   return(
     <div className='text-center py-10'>
-      <div className='text-gray-400 font-semibold font-serif text-sm'>Welcome , to Heer Education Hub</div>
+      <div className='text-gray-400 font-semibold font-serif text-xs sm:text-sm'>Welcome , to Heer Education Hub</div>
         <div className={`transform transition-transform duration-1000 ease-in-out ${zoom ? 'scale-110 text-gray-400' : 'scale-10'}`}>
-      <h1 className='text-6xl font-bold uppercase  py-5'>Admin Table</h1>
+      <h1 className='text-4xl md:text-6xl font-bold uppercase  py-5'>Admin Table</h1>
       </div>
 
-      <section className='flex items-center justify-center flex-col'>
-        <div className='text-gray-400 font-semibold font-serif text-sm'>One Destination for complete Web Development</div>
-        <nav className='bg-gray-100 w-[80%] my-5 p-4'>
-          <ul className='flex items-center justify-center gap-8 capitalize text-gray-600 font-medium '>
+      <section className='flex items-center justify-center flex-col w-full'>
+        <div className='text-gray-400 font-semibold font-serif text-xs sm:text-sm'>One Destination for complete Web Development</div>
+        <nav className='bg-gray-100 w-full sm:max-w-full md:w-[80%] lg:w-[50%] my-5 p-4 mx-auto'>
+          <ul className='flex items-center justify-center  gap-4 sm:gap-8 capitalize text-gray-600 font-medium text-xs sm:text-[15px]'>
             <li>
               <a href="#">Home</a>
            </li>
