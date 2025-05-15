8df65060919b7e071d8884586f9afa2ba87ee55f
Heer Savaliya <heersavaliya65@gmail.com>
2025-05-15 05:53:02 +0530

    small change

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -3,14 +3,14 @@ import DeleteAllUser from "./DeleteAllUser";
 import { fakeUserData } from "../API/Api";
 
 const UserDetails = () => {
-  const addNewUsers = ()=>{``}
+  const addNewUsers = ()=>{}
   return (
     <div className="w-full md:w-[80%] lg:w-[50%]  mx-auto">
       {/* Admin table */}
       <div>
         <div className="flex items-center justify-between my-5">
           <div className="text-gray-500 font-semibold font-serif text-2xl">
-            List of User Details
+            List of User Details 
           </div>
           <button onClick={()=>addNewUsers(fakeUserData())} className="px-5 py-2 rounded-[12px] text-white bg-linear-to-t from-sky-500 to-indigo-500">
             Add New Users
